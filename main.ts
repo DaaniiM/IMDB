02b40010eef69b46a08df31907b0ed0e337d996b
DaaniiM <danimellado1993@gmail.com>
2021-01-28 10:46:16 +0100

    Actualizando

--- main.ts
+++ main.ts
@@ -4,35 +4,35 @@ import {Professional} from "./professional";
 let actor1 = new Professional("Harrison Ford",78,"Male",81,180,"Brown","Brown","Caucasic",false,"North American",0,"Actor");
 let actor2 = new Professional("Vin Diesel",42,"Male",98,184,"NonColor","Brown","Caucasic",false,"North American",0,"Actor");
 let actor3 = new Professional("Tom Holland",24,"Male",75,172,"Red hair","Black","Caucasic",false,"North American",0,"Actor");
-let actor4 = new Professional("Brie Larson",31,"Female",170,58,"Blond","Brown","Caucasic",false,"North American",0,"Actress");
-let actor5 = new Professional("Natalie Portman",39,"Female",160,52,"Brown","Brown","Caucasic",false,"Isreali",1,"Actress");
+let actor4 = new Professional("Brie Larson",31,"Female",58,170,"Blond","Brown","Caucasic",false,"North American",0,"Actress");
+let actor5 = new Professional("Natalie Portman",39,"Female",52,172,"Brown","Brown","Caucasic",false,"Isreali",1,"Actress");
 let actor6 = new Professional("Edward Norton",45,"Male",80,182,"Brown", "Brown","Caucasic", false, "North American", 2,"Actor");
 let actor7 = new Professional("Gabby Hoffman",40,"Female",70,170,"Brown","Brown","Caucasica", false,"North American",0,"Actress");
 let actor8 = new Professional("Bruce Willis",58,"Male", 82,176, "Brown","Brown","Caucasic", false,"Germany",1,"Actor");
-let actor9 = new Professional("Jean Claude Van-damme",60,"Male",8,183,"Black","Brown","Caucasic",false,"Belgium", 2,"Actor");
-let actor10 = new Professional("Penelope Cruz",50,"Female",60,171,"Brown","Brown","Hispanic",false, "Spain",2,"Actress");
+let actor9 = new Professional("Jean Claude Van-damme",60,"Male",89,183,"Black","Brown","Caucasic",false,"Belgium", 2,"Actor");
+let actor10 = new Professional("Penelope Cruz",50,"Female",60,171,"Brown","Brown","Caucasic",false, "Spanish",2,"Actress");
 
 let director1 = new Professional("Steven Spielberg",74,"Male",90,172,"White","Brown","Caucasic",false,"North American",4,"Director");
-let director2 = new Professional("Thomas Williams",54,"Male",182,80,"Black","Black","Caucasic",false,"British",1,"Director");
-let director3 = new Professional("Andrea Gonzalez",42,"Female",174,60,"Brown","Brown","Caucasic",false,"Spanish",0,"Director");
-let director4 = new Professional("Andrew Hoffman",51,"Male",184,80,"Black","Brown","Caucasic",false,"North American",3,"Director");
-let director5 = new Professional("Anthony Loster",68,"Male",179,80,"White","Brown","Caucasic",true,"British",1,"Director");
+let director2 = new Professional("Thomas Williams",54,"Male",80,182,"Black","Black","Caucasic",false,"British",1,"Director");
+let director3 = new Professional("Andrea Gonzalez",42,"Female",60,174,"Brown","Brown","Caucasic",false,"Spanish",0,"Director");
+let director4 = new Professional("Andrew Hoffman",51,"Male",80,184,"Black","Brown","Caucasic",false,"North American",3,"Director");
+let director5 = new Professional("Anthony Loster",68,"Male",80,179,"White","Brown","Caucasic",true,"British",1,"Director");
 let director6 = new Professional("Paco Castillejo",90,"Male",78,187,"Blonde","Blue","Caucasic",true,"Argentina",7,"Directo");
-let director7 = new Professional("Justyna Kotwica",24,"Female",65,176,"Blonde","Blue","caucasic",false,"Poland",4,"Director");
+let director7 = new Professional("Justyna Kotwica",24,"Female",65,176,"Blonde","Blue","Caucasic",false,"Poland",4,"Director");
 let director8 = new Professional("Camila Potterback",45,"Female",56,165,"Brown","Blue","Caucasic", false,"Irland",4, "Director");
-let director9 = new Professional("Ming janh-haulasgop",68,"Male",78,178,"Brown","Green","Asian",true,"Thailand",24,"Director");
-let director10 = new Professional("Otumanda Hamandobom",48,"Male", 90,200,"black","Black","Congo",false,"African",5,"Director");
+let director9 = new Professional("Ming Janh-Haulasgop",68,"Male",78,178,"Brown","Green","Asian",true,"Thailand",24,"Director");
+let director10 = new Professional("Otumanda Hamandobom",48,"Male",90,200,"Black","Black","African",false,"Congo",5,"Director");
 
 let producer1 = new Professional("Harvey Weinstein",68,"Male",85,183,"Brown","Black","Caucasic",true,"North American",0,"Producer");
-let producer2 = new Professional("Angelina Hopkins",52,"Female",171,60,"Black","Black","Caucasico",false,"North American",0,"Producer");
-let producer3 = new Professional("Francisco Menendez",71,"Male",184,94,"White","Brown","Caucasic",false,"Spanish",1,"Producer");
-let producer4 = new Professional("John McCallahan",41,"Male",190,98,"Blond","Black","Caucasic",true,"North American",0,"Producer");
-let producer5 = new Professional("Nikita Volkov",49,"Female",172,65,"Black","Brown","Caucasic",true,"Russian",0,"Producer");
-let producer6 = new Professional("Faustina Peckman",60,"Female",56,167,"White","blue","Caucaisc",false,"England",2,"Producer");
+let producer2 = new Professional("Angelina Hopkins",52,"Female",60,171,"Black","Black","Caucasic",false,"North American",0,"Producer");
+let producer3 = new Professional("Francisco Menendez",71,"Male",94,184,"White","Brown","Caucasic",false,"Spanish",1,"Producer");
+let producer4 = new Professional("John McCallahan",41,"Male",98,190,"Blond","Black","Caucasic",true,"North American",0,"Producer");
+let producer5 = new Professional("Nikita Volkov",49,"Female",65,172,"Black","Brown","Caucasic",true,"Russian",0,"Producer");
+let producer6 = new Professional("Faustina Peckman",60,"Female",56,167,"White","Blue","Caucasic",false,"England",2,"Producer");
 let producer7 = new Professional("Kamil Wojkowski",34,"Male",83,193,"Blonde","Blue","Caucasic",false,"Poland",3,"Producer");
-let producer8 = new Professional("Luisa Parra",46,"Female",67,176,"Brown","brown","Hispanic",false,"Spain",1,"Produder");
-let producer9 = new Professional("Jefferson Olsten-gambern",89,"Male",78,190,"white","Blue","Caucasic",true,"Island",13,"producer");
-let producer10 = new Professional("Tomas Turbado",23,"Male",80,181,"White","Brown","Causic",false,"Spain",2,"Producer"); 
+let producer8 = new Professional("Luisa Parra",46,"Female",67,176,"Brown","Brown","Caucasic",false,"Spanish",1,"Producer");
+let producer9 = new Professional("Jefferson Olsten-Gambern",89,"Male",78,190,"White","Blue","Caucasic",true,"Island",13,"Producer");
+let producer10 = new Professional("Tomas Thompson",23,"Male",80,181,"White","Brown","Caucasic",false,"Spanish",2,"Producer"); 
 
 
 
