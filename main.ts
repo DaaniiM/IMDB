464286c304e1b642539e2897db2c5185a4728b5f
PedroCorcho <corchogamez.pedro@gmail.com>
2021-01-28 10:03:02 +0100

    Subiendo Parte Pedro

--- main.ts
+++ main.ts
@@ -2,10 +2,25 @@
 import {Professional} from "./professional";
 
 let actor1 = new Professional("Harrison Ford",78,"Male",81,180,"Brown","Brown","Caucasic",false,"North American",0,"Actor");
+let actor6 = new Professional("Edward Norton",45,"Male",80,182,"Brown", "Brown","Caucasic", false, "North American", 2,"Actor");
+let actor7 = new Professional("Gabby Hoffman",40,"Female",70,170,"Brown","Brown","Caucasica", false,"North American",0,"Actress");
+let actor8 = new Professional("Bruce Willis",58,"Male", 82,176, "Brown","Brown","Caucasic", false,"Germany",1,"Actor");
+let actor9 = new Professional("Jean Claude Van-damme",60,"Male",8,183,"Black","Brown","Caucasic",false,"Belgium", 2,"Actor");
+let actor10 = new Professional("Penelope Cruz",50,"Female",60,171,"Brown","Brown","Hispanic",false, "Spain",2,"Actress");
 
 let director1 = new Professional("Steven Spielberg",74,"Male",90,172,"White","Brown","Caucasic",false,"North American",4,"Director");
+let director6 = new Professional("Paco Castillejo",90,"Male",78,187,"Blonde","Blue","Caucasic",true,"Argentina",7,"Directo");
+let director7 = new Professional("Justyna Kotwica",24,"Female",65,176,"Blonde","Blue","caucasic",false,"Poland",4,"Director");
+let director8 = new Professional("Camila Potterback",45,"Female",56,165,"Brown","Blue","Caucasic", false,"Irland",4, "Director");
+let director9 = new Professional("Ming janh-haulasgop",68,"Male",78,178,"Brown","Green","Asian",true,"Thailand",24,"Director");
+let director10 = new Professional("Otumanda Hamandobom",48,"Male", 90,200,"black","Black","Congo",false,"African",5,"Director");
 
 let producer1 = new Professional("Harvey Weinstein",68,"Male",85,183,"Brown","Black","Caucasic",true,"North American",0,"Producer");
+let producer6 = new Professional("Faustina Peckman",60,"Female",56,167,"White","blue","Caucaisc",false,"England",2,"Producer");
+let producer7 = new Professional("Kamil Wojkowski",34,"Male",83,193,"Blonde","Blue","Caucasic",false,"Poland",3,"Producer");
+let producer8 = new Professional("Luisa Parra",46,"Female",67,176,"Brown","brown","Hispanic",false,"Spain",1,"Produder");
+let producer9 = new Professional("Jefferson Olsten-gambern",89,"Male",78,190,"white","Blue","Caucasic",true,"Island",13,"producer");
+let producer10 = new Professional("Tomas Turbado",23,"Male",80,181,"White","Brown","Causic",false,"Spain",2,"Producer"); 
 
 actor1.showDetails();
 director1.showDetails();
