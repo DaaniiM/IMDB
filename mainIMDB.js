1dded8b5386997e616550f69ba6eb4abed43525b
DaaniiM <danimellado1993@gmail.com>
2021-01-29 08:38:05 +0100

    Base de datos Peliculas

--- mainIMDB.js
+++ mainIMDB.js
@@ -3,4 +3,12 @@ exports.__esModule = true;
 var IMDB_1 = require("./IMDB");
 var mainMovies_1 = require("./mainMovies");
 var peliculasAll = new IMDB_1.Imdb([mainMovies_1.pelicula1, mainMovies_1.pelicula2, mainMovies_1.pelicula3]);
+/*
+
 console.log(peliculasAll);
+
+*/
+var peliculasJson = JSON.stringify(peliculasAll);
+console.log(peliculasJson);
+var fs = require('fs');
+fs.writeFileSync('imdbBBDD.json', peliculasJson);
--- mainIMDB.ts
+++ mainIMDB.ts
@@ -5,5 +5,16 @@ import { pelicula1, pelicula2, pelicula3 } from "./mainMovies";
 
 let peliculasAll = new Imdb([pelicula1,pelicula2,pelicula3]);
 
+/*
+
 console.log(peliculasAll);
 
+*/
+
+let peliculasJson = JSON.stringify(peliculasAll);
+
+
+const fs = require('fs')
+
+
+fs.writeFileSync('imdbBBDD.json', peliculasJson);
